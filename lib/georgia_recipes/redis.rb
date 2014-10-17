b73dd81cb60e3f52e5ab076815c222aa320d7164
Mathieu Gagné <mgagne@vigilantglobal.com>
2014-10-17 09:30:51 -0400

    Add redis:setup with init file

--- lib/georgia_recipes/redis.rb
+++ lib/georgia_recipes/redis.rb
@@ -8,6 +8,14 @@ Capistrano::Configuration.instance.load do
       run "#{sudo} apt-get -y install redis-server"
     end
 
+
+    desc "Setup redis-server"
+    task :setup, roles: :web do
+      run "#{sudo} update-rc.d redis-server disable"
+      template("redis.erb", "/tmp/redis_conf")
+      run "#{sudo} mv -u /tmp/redis_conf /etc/init/redis-server.conf"
+    end
+
     desc "Uninstall Redis"
     task :uninstall, roles: :app do
       run "#{sudo} apt-get -y remove redis-server"
@@ -16,6 +24,12 @@ Capistrano::Configuration.instance.load do
     desc "Start Redis Server"
     task :start, roles: :app do
       run "redis-server"
+    end
+
+    %w(start stop restart).each do |command|
+      task command, roles: :app do
+        run "#{sudo} #{command} redis-server"
+      end
     end
 
   end
