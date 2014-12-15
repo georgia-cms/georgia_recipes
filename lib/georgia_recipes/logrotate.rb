c93f772f9f921471f2d6087febee63b8c9cfd7db
Mathieu Gagné <mgagne@vigilantglobal.com>
2014-12-15 08:44:46 -0500

    Ensures logrotate config is owned by root

--- lib/georgia_recipes/logrotate.rb
+++ lib/georgia_recipes/logrotate.rb
@@ -1,10 +1,17 @@
 Capistrano::Configuration.instance.load do
 
   namespace :logrotate do
+
     desc "Setup log rotation for Rails app log files"
     task :setup, roles: :web do
       template("logrotate.erb", "/tmp/logrotate_conf")
       run "#{sudo} mv -u /tmp/logrotate_conf /etc/logrotate.d/#{application}"
+      run "#{sudo} chown root:root /etc/logrotate.d/#{application}"
+    end
+
+    desc "Runs logrotate with default config file"
+    task :start, roles: :web do
+      run "#{sudo} logrotate /etc/logrotate.conf"
     end
   end
 
